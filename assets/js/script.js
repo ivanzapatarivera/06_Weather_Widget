72f25f5d2fcbfb0b31fe1a68d32fad0f04c7a69c
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-04 19:03:07 -0400

    creating object for key

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,26 +5,48 @@ $(document).ready(function() {
 // Obtain and display search criteria on navbar with class searchDeposit
 // The criteria is contained inside input with class searchWeather
 
+var cityST = $('#cityST');
 var submit = $('.submitSearch');
 var deposit = $('.searchDeposit');
-var searchWeather = $('#searchWeather')
-    console.log(searchWeather);
+var searchCityST = $('#searchCityST')
+    
 
+
+// Appends search into top bar
 submit.on('click', function(){
 
     event.preventDefault();
     appendCity();
-
+    addKey();
 
 })
 
 
+
+citySTObj = {};
+
 function appendCity() {
 
     event.preventDefault();
-    deposit.append('<div class="d-inline ml-4">' + searchWeather.val() + '</div>');
+    deposit.append('<div class="d-inline ml-4">' + searchCityST.val() + '</div>');
+    var result = searchCityST.val();
+    citySTObj.push(result);
+}
+console.log(citySTObj);
+
+
+function addKey() {
+
+    var value = searchCityST.val()
+    console.log(value);
+    var key = cityST;
+    console.log(key);
+    localStorage.setItem(key, value);
 
 }
+
+
+
 
 // Obtain the AJAX for OpenWeather
 // Include Search's AJAX details into appendCity();
