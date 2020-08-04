066de107151a122ad399b949c65c8f4bc475b3df
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-04 17:53:22 -0400

    added styles and script to add city search

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
 
-   
+
+// Create onclick function for button with class submitSearch
+// Obtain and display search criteria on navbar with class searchDeposit
+// The criteria is contained inside input with class searchWeather
+
+var submit = $('.submitSearch');
+var deposit = $('.searchDeposit');
+var searchWeather = $('#searchWeather')
+    console.log(searchWeather);
+
+submit.on('click', function(){
+
+    event.preventDefault();
+    appendCity();
 
 
-    
+})
+
+
+function appendCity() {
+
+    event.preventDefault();
+    deposit.append('<div class="d-inline ml-4">' + searchWeather.val() + '</div>');
+
+}
+
+// Obtain the AJAX for OpenWeather
+// Include Search's AJAX details into appendCity();
 
 })
