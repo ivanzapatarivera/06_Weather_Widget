a5c4f14587f72ade43a9eb220855a44b00aa1bac
ivanzapatarivera <ivan.zapata.rivera@gmail.com>
2020-08-04 23:53:32 -0400

    added styles, edited script.js and index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,8 @@ var submit = $('.submitSearch');
 var deposit = $('.searchDeposit');
 var searchCity = $('#searchCity');
 var searchCityClass = $('.searchCity');
-    
+
+
 
 // Establishes Event Listener Function
 submit.on('click', function(){
@@ -19,6 +20,8 @@ submit.on('click', function(){
     appendCity();
     addLocalStorage();
     apiAJAXCall();
+
+    console.log("You searched for city: " + searchCity.val());
 
 })
 
@@ -33,34 +36,50 @@ cityObj = {
 function appendCity() {
 
     event.preventDefault();
-    deposit.append('<div class="d-inline ml-4">' + searchCity.val() + '</div>');
+    deposit.append('<div class="d-inline ml-4 resultCity">' + searchCity.val() + '</div>');
     var result = searchCity.val();
     cityObj.cityArray.push(result);
+    resultCity();
 
 }
 
+function resultCity() {
+
+$('.resultCity').on('click', function() {
+    alert("Weather forecast from previous search.")
+})
+
+}
 
 console.log(cityObj);
 
 // Creates key and value with array within object (multiple city entries)
 function addLocalStorage() {
-
+    event.preventDefault();
     var key = searchCityClass.parent().attr("id");
     console.log(key);
     var value = cityObj.cityArray;
     console.log(value);
     localStorage.setItem(key, value);
-
+    
 }
 
+$('#searchDeposit .resultCity').val(localStorage.getItem('city'));
 
 // Query URL to pull city data
 
-var apiKey = '153c22b1d7bf8797ae2dad9664ef02d7';
-var queryURL = 'http://pro.openweathermap.org/data/2.5/forecast/hourly?q=' + 'London' + '&appid=' + apiKey;
+
+
+
 
 // AJAX call 
 function apiAJAXCall() {
+
+console.log("Search City Value is: " + searchCity.val());
+var apiKey = '3f0e791b672eddc26b02cdefef533281';
+var queryURL = 'http://api.openweathermap.org/data/2.5/forecast/daily?q=' + searchCity.val() + '&appid=' + apiKey;
+
+
 $.ajax({
     url: queryURL,
     method: "GET"
